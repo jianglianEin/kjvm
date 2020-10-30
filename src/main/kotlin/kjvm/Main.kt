eae4b4b3a1b36dd15f2395377f97abe3dec22970
jianglianEin <913057041@qq.com>
2020-10-30 16:18:08 +0800

    [jianglianEin] init simple jvm project written by kotlin.

---
+++ src/main/kotlin/kjvm/Main.kt
@@ -0,0 +1,10 @@
+package kjvm
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) { // write your code here
+            println("hello world")
+        }
+    }
+}
